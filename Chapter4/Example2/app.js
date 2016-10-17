4646e3548c6076f476cc9c5005ced21b537e83f4
Lance Johnson <git@gloryandjoy.com>
2016-10-17 10:42:42 -0500

    Add Check for Blank Input

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,14 +1,14 @@
 var main = function() {
 	"use strict";
+	
 
 	$(".comment-input button").on("click", function (event) {
-		var $new_comment = $("<p>"),
-		comment_text = $(".comment-input input").val();
-				
-		$new_comment.text(comment_text);
-		console.log($new_comment);
-		$(".comments").append($new_comment);
+		var $new_comment;
 		
+		if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);
+		}		
 	});
 };
 
