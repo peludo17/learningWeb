be7a53da738ffdecd46853c60a7444d4e13c951f
Lance Johnson <git@gloryandjoy.com>
2016-10-18 09:11:43 -0500

    Add js First Level

--- Chapter4/Amazeriffic/javascripts/app.js
+++ Chapter4/Amazeriffic/javascripts/app.js
@@ -1,7 +1,47 @@
 var main = function() {
 	"use strict";
 	
-	console.log("Hello World!");
+	$(".tabs a:nth-child(1)").on("click", function () {
+		// make all the tabs inactive
+		$(".tabs span").removeClass("active");
+		
+		// make the first tab active
+		$(".tabs a:nth-child(1) span").addClass("active");
+		
+		// empty the main content so we can recreate it
+		$("main .content").empty();
+		
+		// return false so we don't follow the link
+		return false;
+	});
+	
+	$(".tabs a:nth-child(2)").on("click", function () {
+		// make all the tabs inactive
+		$(".tabs span").removeClass("active");
+		
+		// make the first tab active
+		$(".tabs a:nth-child(2) span").addClass("active");
+		
+		// empty the main content so we can recreate it
+		$("main .content").empty();
+		
+		// return false so we don't follow the link
+		return false;
+	});
+	
+	$(".tabs a:nth-child(3)").on("click", function () {
+		// make all the tabs inactive
+		$(".tabs span").removeClass("active");
+		
+		// make the first tab active
+		$(".tabs a:nth-child(3) span").addClass("active");
+		
+		// empty the main content so we can recreate it
+		$("main .content").empty();
+		
+		// return false so we don't follow the link
+		return false;
+	});
 };
 
 $(document).ready(main);
