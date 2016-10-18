3e4785a57d49d455b04a9dd900825c2fc9c2e006
Lance Johnson <git@gloryandjoy.com>
2016-10-18 09:25:51 -0500

    Add refactored js

--- Chapter4/Amazeriffic/javascripts/app.js
+++ Chapter4/Amazeriffic/javascripts/app.js
@@ -1,46 +1,14 @@
 var main = function() {
 	"use strict";
 	
-	$(".tabs a:nth-child(1)").on("click", function () {
-		// make all the tabs inactive
-		$(".tabs span").removeClass("active");
-		
-		// make the first tab active
-		$(".tabs a:nth-child(1) span").addClass("active");
-		
-		// empty the main content so we can recreate it
-		$("main .content").empty();
-		
-		// return false so we don't follow the link
-		return false;
-	});
-	
-	$(".tabs a:nth-child(2)").on("click", function () {
-		// make all the tabs inactive
-		$(".tabs span").removeClass("active");
-		
-		// make the first tab active
-		$(".tabs a:nth-child(2) span").addClass("active");
-		
-		// empty the main content so we can recreate it
-		$("main .content").empty();
-		
-		// return false so we don't follow the link
-		return false;
-	});
-	
-	$(".tabs a:nth-child(3)").on("click", function () {
-		// make all the tabs inactive
-		$(".tabs span").removeClass("active");
-		
-		// make the first tab active
-		$(".tabs a:nth-child(3) span").addClass("active");
-		
-		// empty the main content so we can recreate it
-		$("main .content").empty();
-		
-		// return false so we don't follow the link
-		return false;
+	$(".tabs span").toArray().forEach(function (element) {
+		// create a click handler for this element
+		$(element).on("click", function() {
+			$(".tabs span").removeClass("active");
+			$(element).addClass("active");
+			$("main .content").empty();
+			return false;
+		});
 	});
 };
 
