ff95ce1751cc424da557f68078d7f5c8746d6a7b
Lance Johnson <git@gloryandjoy.com>
2016-11-17 08:55:23 -0600

    JS running

--- Chapter4/Amazeriffic/javascripts/app.js
+++ Chapter4/Amazeriffic/javascripts/app.js
@@ -1,15 +1,7 @@
 var main = function() {
 	"use strict";
-	
-	$(".tabs span").toArray().forEach(function (element) {
-		// create a click handler for this element
-		$(element).on("click", function() {
-			$(".tabs span").removeClass("active");
-			$(element).addClass("active");
-			$("main .content").empty();
-			return false;
-		});
-	});
+
+	console.log("hello world!");
 };
 
 $(document).ready(main);
